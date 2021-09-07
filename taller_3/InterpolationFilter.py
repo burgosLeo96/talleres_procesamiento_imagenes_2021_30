16226ac0e36a4caebdbcb6d0ebf07524a3dae1d9
BurgosLeo96 <burgosleonard96.lb@gmail.com>
2021-09-06 19:29:44 -0500

    Added comments in InterpolationFilter.py file

--- taller_3/InterpolationFilter.py
+++ taller_3/InterpolationFilter.py
@@ -9,6 +9,7 @@ def interpolation_fft(image, i_value):
 
     i_value = int(i_value) - 1
 
+    # Calculate gray scale image if necessary
     if len(image.shape) == 3:
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     else:
@@ -16,14 +17,16 @@ def interpolation_fft(image, i_value):
 
     num_rows, num_columns = gray_image.shape
 
+    # Compute interpolated image: start by creating an empty image
     zeros_image = np.zeros((i_value * num_rows, i_value * num_columns), dtype=gray_image.dtype)
     zeros_image[::i_value, ::i_value] = gray_image
     window_size = 2 * i_value + 1
 
+    # Interpolate the missing pixels values
     interpolated_image = cv2.GaussianBlur(zeros_image, (window_size, window_size), 1.0)
     interpolated_image *= zeros_image ** 2
 
-    # First of all, make operations over Fourier transform
+    # Filter frequencies over interpolated image
     gray_image, image_fft_view, image_filtered, fft_filtered_view = compute_fourier_transform(interpolated_image, i_value)
 
     return image_filtered
